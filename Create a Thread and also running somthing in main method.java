7a3a62bbcd0335391b1cc0c2f75262ef8bebc3a3
Arman Hossain <64675035+Arman170616@users.noreply.github.com>
2021-09-11 10:09:49 +0600

    Update Create a Thread and also running somthing in main method.java

--- Create a Thread and also running somthing in main method.java
+++ Create a Thread and also running somthing in main method.java
@@ -1,1 +1,30 @@
+import java.util.concurrent.TimeUnit;
+import java.io.*;
+
+
+public class lab_05 extends Thread {
+
+    public void run(){
+        for (int i = 0; i <= 10; i++){
+            System.out.println("Problem_05");
+            try {
+                TimeUnit.SECONDS.sleep(1);
+
+            }catch(InterruptedException e){
+                e.printStackTrace();
+
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException{
+        lab_05 thread = new lab_05();
+        thread.run();
+        for (int i = 0; i <=10; i++){
+            System.out.println(" "+i);
+            TimeUnit.SECONDS.sleep(1);
+        }
+    }
+
+}
 
